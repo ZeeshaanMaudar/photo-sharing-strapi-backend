0d9d39ff08cec35e51a2696048f1f09f7967fe66
Zeeshaan Maudarbocus <zeeshaan@touchfoundry.co.za>
2020-08-24 21:50:36 +0200

    set up strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '56f71bdb94ad7701ca7afdc94f0e472a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd71fb9cf-5216-4149-84ee-a4228814fee1'
+};
